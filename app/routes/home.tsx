7a63a5df6327735c09fbb399549676c81938af17
amir <amirtaki623@gmail.com>
2025-04-28 01:49:05 +0330

    class part1 tw

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,25 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+// import type { Route } from "./+types/home";
+// import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: "New React Router App" },
+//     { name: "description", content: "Welcome to React Router!" },
+//   ];
+// }
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline ">
-    Hello world!
-  </h1>
+    <>
+      <h1 className="text-center  text-xl font-medium text-orange-500 bg-amber-950">
+        Hello world!
+      </h1>
+    
+      <h2 className="text-center font-bold text-5xl">product</h2>
+   
+   
+    </>
+  
 
   );
   
