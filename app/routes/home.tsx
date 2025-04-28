596ea22b5ac55fcfc0fffffa83f80eb714182536
amir <amirtaki623@gmail.com>
2025-04-28 14:08:49 +0330

    effect in tw

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,18 +12,18 @@ export default function Home() {
   return (
     <>
 
-    {/* screen */}
+    {/* effect */}
 
 
     <div  className="bg-green-800  h-96 grid grid-cols-12  gap-4  xl:hidden">
         
-        <div className="bg-orange-900 h-32 col-span-12 sm:col-span-6  md:col-span-3 "></div>
+        <div className="bg-orange-900 hover:bg-red-100 h-32 col-span-12 sm:col-span-6  md:col-span-3 "></div>
   
         <div className="bg-orange-500 h-32 col-span-12 sm:col-span-6  md:col-span-3 "></div>
   
-        <div className="bg-orange-200 h-32 col-span-12 sm:col-span-6  md:col-span-3"></div>
+        <div className="bg-orange-200 h-32 hover:h-10 col-span-12 sm:col-span-6  md:col-span-3"></div>
   
-        <div className="bg-orange-100 h-32 col-span-12 sm:col-span-6  md:col-span-3"></div>
+        <div className="bg-orange-100 active:bg-red-950 h-32 col-span-12 sm:col-span-6  md:col-span-3"></div>
   
     </div>
     
