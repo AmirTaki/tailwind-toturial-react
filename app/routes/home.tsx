965cd2c89ac8eaf24171ba89c6c9003a095ac1a8
amir <amirtaki623@gmail.com>
2025-04-28 14:04:33 +0330

    screen in tw

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,18 +12,19 @@ export default function Home() {
   return (
     <>
 
-    {/* grid */}
+    {/* screen */}
 
 
-    <div 
-      className="bg-green-800  h-96 grid grid-cols-4  gap-4">
+    <div  className="bg-green-800  h-96 grid grid-cols-12  gap-4  xl:hidden">
         
-        <div className="bg-orange-900 h-32 col-span-2"></div>
-        <div className="bg-orange-500 h-32"></div>
-        <div className="bg-orange-200 h-32"></div>
-        <div className="bg-orange-100 h-32 col-span-3"></div>
-    
-    
+        <div className="bg-orange-900 h-32 col-span-12 sm:col-span-6  md:col-span-3 "></div>
+  
+        <div className="bg-orange-500 h-32 col-span-12 sm:col-span-6  md:col-span-3 "></div>
+  
+        <div className="bg-orange-200 h-32 col-span-12 sm:col-span-6  md:col-span-3"></div>
+  
+        <div className="bg-orange-100 h-32 col-span-12 sm:col-span-6  md:col-span-3"></div>
+  
     </div>
     
 
@@ -36,37 +37,13 @@ export default function Home() {
     <br></br>
     <br></br>
 
-    {/* flex box */}
-
-    <div 
-      className="bg-gray-800  h-96 flex  justify-between items-center">
-        
-        <div className="bg-orange-500 size-32"></div>
-        <div className="bg-orange-500 size-32"></div>
-        <div className="bg-orange-500 size-32"></div>
-        <div className="bg-orange-500 size-32"></div>
     
-    
-    </div>
-    
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-
-    {/* flex - row */}
-
-    <div className="bg-blue-800  h-96  flex  flex-row">
-
-    <div className="bg-gray-500 h-32 basis-1/12"></div>
-    <div className="bg-red-500 h-32 basis-2/12"></div>
-    <div className="bg-orange-500 h-32 basis-4/12"></div>
-    <div className="bg-green-500 h-32 basis-2/12"></div>
-
-    </div>
       
    
-   
+    <div className="sm-text-lg lg:text-4xl">
+      screen responsive
+      </div>  
+
     </>
   
 
