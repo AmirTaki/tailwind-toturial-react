fbb1fb9bc816d16643d82f17fba2b500fb6b753f
amir <amirtaki623@gmail.com>
2025-04-28 13:32:46 +0330

    class part3 tw

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,22 +13,57 @@ export default function Home() {
     <>
 
     <div 
-      className="container">
+      className="w-15 bg-zinc-700 h-40 relative bg-red-700 z-index">
     <h1 
       className="
+      rotate-180
+      right-0
+      fixed
+      overflow
+      w-24
       rounded-3xl
-      h-30
-      w-full
-      my-13
-      py-12
+      h-50
+      my-10
+      p-20
       text-center 
-      text-xl 
+      text-2xl 
       font-medium
       text-red-500
       bg-slate-300
-      shadow-blue-500">
+      shadow-blue-500
+      inline-flex
+  
+      ">
         Hello world!
       </h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
     
     
